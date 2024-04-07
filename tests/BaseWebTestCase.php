07a1619f721743dde596dab0879b8b20b0672c8f
Jakub Ćwikowski <kcwikowski007@yahoo.com>
2024-04-07 12:52:55 +0200

    fix: phpunit fix (remove shyim/testcontainer)

--- tests/BaseWebTestCase.php
+++ tests/BaseWebTestCase.php
@@ -5,81 +5,10 @@ namespace App\Tests;
 use Doctrine\DBAL\Connection;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Process\Exception\ProcessFailedException;
-use Symfony\Component\Process\Process;
-use Testcontainer\Container\PostgresContainer;
-
 
 abstract class BaseWebTestCase extends WebTestCase
 {
-  protected static PostgresContainer $postgresContainer;
   protected static Connection $connection;
-
-  public static function setUpBeforeClass(): void
-  {
-    parent::setUpBeforeClass();
-    $postgresVersion = $_ENV['POSTGRES_VERSION'];
-    $postgresUser = $_ENV['POSTGRES_USER'];
-    $postgresDatabase = $_ENV['POSTGRES_DATABASE'];
-    $postgresDatabaseSuffix = $_ENV['POSTGRES_DATABASE_SUFFIX'];
-    $postgresPassword = $_ENV['POSTGRES_PASSWORD'];
-
-    self::$postgresContainer = PostgresContainer::make($postgresVersion, $postgresPassword)
-      ->withPostgresUser($postgresUser)
-      ->withPostgresDatabase($postgresDatabase . $postgresDatabaseSuffix);
-    // remember to pull the image before trying to run container if you have slow internet connection
-    self::$postgresContainer->run();
-
-    self::updateDatabaseUrl(
-      $postgresUser,
-      $postgresPassword,
-      $postgresDatabase . $postgresDatabaseSuffix,
-      $postgresVersion
-    );
-
-    self::runDoctrineMigrations();
-
-    self::updateDatabaseUrl($postgresUser, $postgresPassword, $postgresDatabase, $postgresVersion);
-  }
-
-  private static function updateDatabaseUrl(
-    string $postgresUser,
-    string $postgresPassword,
-    string $postgresDatabase,
-    string $postgresVersion
-  ): void {
-    $_ENV['DATABASE_URL'] = sprintf(
-      'postgresql://%s:%s@%s:%d/%s?serverVersion=%s&charset=utf8',
-      $postgresUser,
-      $postgresPassword,
-      self::$postgresContainer->getAddress(),
-      5432,
-      $postgresDatabase,
-      $postgresVersion
-    );
-  }
-
-  private static function runDoctrineMigrations(): void
-  {
-    $process = new Process([
-      'php',
-      'bin/console',
-      'doctrine:migrations:migrate',
-      '--no-interaction',
-    ]);
-
-    $process->run();
-
-    if (!$process->isSuccessful()) {
-      throw new ProcessFailedException($process);
-    }
-  }
-
-  public static function tearDownAfterClass(): void
-  {
-    parent::tearDownAfterClass();
-    self::$postgresContainer->stop();
-  }
 
   protected function assertJsonResponseHeaders(Response $response): void
   {
